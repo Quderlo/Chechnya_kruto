ba6693f332c946696dbc2948e9630c8c7c210a0b
Quderlo <4546436@gmail.com>
2024-04-17 03:09:09 +0500

    startlevapp

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-
 from users.views.views import HomeStub
 
 urlpatterns = [
@@ -11,5 +10,6 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
     path('users/', include('users.urls')),
     path('api/v0/', include('api_v0.urls')),
+    path('api/v47/', include('api_v47.urls')),
     path('', HomeStub.as_view(), name='homepage'),
 ]
